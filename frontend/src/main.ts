0648ae375e7625b6e0eb8b1959b9d7630e5ca53b
Harald <Harald@espinagar.se>
2023-02-27 16:42:25 +0100

    added a virus that pops up on the screen at a random time between 1-10 seconds

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -34,3 +34,30 @@ if (joinGameBtn && lobbyEl && gameEl) {
 		gameEl.style.display = 'block'
 	})
 }
+
+let virusTimeout: number;
+
+function displayVirus() {
+	const cells = document.querySelectorAll('.cell');
+	const randomIndex = Math.floor(Math.random() * cells.length);
+	const randomCell = cells[randomIndex];
+
+	// Remove any existing virus emoji
+	const existingVirus = document.querySelector('.cell-virus');
+	if (existingVirus) {
+		existingVirus.textContent = '';
+		existingVirus.classList.remove('cell-virus');
+	}
+
+	// Display the new virus emoji
+	randomCell.textContent = '🦠';
+	randomCell.classList.add('cell-virus');
+
+	// Schedule the next virus display
+	const randomTime = Math.floor(Math.random() * 10) + 1;
+	virusTimeout = setTimeout(displayVirus, randomTime * 1000);
+}
+
+// Schedule the first virus display
+const randomTime = Math.floor(Math.random() * 10) + 1;
+virusTimeout = setTimeout(displayVirus, randomTime * 1000);
