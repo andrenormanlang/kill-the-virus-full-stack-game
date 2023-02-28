4bcbde8c3c94744285a5d3f7648711f1b3d5887c
Harald <Harald@espinagar.se>
2023-02-28 17:43:29 +0100

    moved enter username to lobby screen insted

--- frontend/src/assets/ts/virus.ts
+++ frontend/src/assets/ts/virus.ts
@@ -1,26 +1,7 @@
 export { }
-
-const usernameForm = document.querySelector<HTMLFormElement>('#usernameForm')
-const inputEl = document.querySelector<HTMLInputElement>('#input')
 const lobbyEl = document.querySelector<HTMLDivElement>('#lobby')
 
-if (usernameForm && inputEl && lobbyEl) {
-	usernameForm.addEventListener('submit', (e) => {
-		e.preventDefault()
-
-		const username = inputEl.value.trim()
-
-		if (username) {
-			// Hide the username input
-			usernameForm.style.display = 'none'
-
-			// Show the lobby
-			lobbyEl.style.display = 'block'
-		}
-	})
-}
-
-const joinGameBtn = document.querySelector<HTMLButtonElement>('#joinGame')
+const joinGameBtn = document.querySelector<HTMLButtonElement>('#enter')
 const gameEl = document.querySelector<HTMLDivElement>('#game')
 
 if (joinGameBtn && lobbyEl && gameEl) {
