4d2de0f0ae7a6d7e98fe7683051263526157bad4
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-28 16:59:41 +0100

    createGameRoom socketType test

--- backend/src/types/shared/socket_types.ts
+++ backend/src/types/shared/socket_types.ts
@@ -4,9 +4,13 @@
 export {}
 
 // Events emitted by the server to the client
-export interface ServerToClientEvents {}
+export interface ServerToClientEvents {
+	createGameRoom: () => void
+}
 
 // Events emitted by the client to the server
-export interface ClientToServerEvents { }
+export interface ClientToServerEvents {
+
+}
 
 export interface InterServerEvents { }
--- frontend/src/assets/ts/socket.ts
+++ frontend/src/assets/ts/socket.ts
@@ -1,10 +1,15 @@
 import { io, Socket } from 'socket.io-client'
+import { ClientToServerEvents, ServerToClientEvents} from '../../../../backend/src/types/shared/socket_types'
 
 const SOCKET_HOST = import.meta.env.VITE_APP_SOCKET_HOST
 const socket = io(SOCKET_HOST)
 
 socket.on('connect', () => {
 	console.log('hej')
+})
+
+socket.on('createGameRoom', () => {
+	console.log('creating game room')
 })
 
 export default socket
